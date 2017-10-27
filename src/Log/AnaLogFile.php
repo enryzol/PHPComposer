c9e6ca9cec25bab2121eb325d56ad309613056a7
Hjay@office <hjie1125@sina.com>
2017-10-27 13:51:37 +0800

    日志修复

--- src/Log/AnaLogFile.php
+++ src/Log/AnaLogFile.php
@@ -16,13 +16,22 @@ namespace HjCommon\Log;
  * Note: Uses Analog::$format for the appending format.
  */
 class AnaLogFile {
-    public static $format = "\n[%s][URL:%s][IP:%s][LEVEL:%d]:\n=> %s\n";
+    public static $format = "\n[%s][IP:%s][URL:%s]:\n=> %s\n";
     public static $format_after = "=> %s\n";
     
 	public static function init ($file) {
 	    global $Analog_File_Write;
 		return function ($info, $buffered = false) use ($file) {
 		    global $Analog_File_Write;
+
+		    $log = array (
+		        'date' => $info['date'],
+		        'machine' => $info['machine'],
+		        'url' => $_SERVER['REQUEST_URI'],
+// 		        'level' => $info['level'],
+		        'message' => $info['message']
+		    );
+		    
 			$f = fopen ($file, 'a+');
 			if (! $f) {
 				throw new \LogicException ('Could not open file for writing');
@@ -36,16 +45,16 @@ class AnaLogFile {
 			    $Analog_File_Write[$file] = true;
 			    fwrite ($f, ($buffered)
 			        ? $info
-			        : vsprintf (AnaLogFile::$format, $info));
+			        : vsprintf (AnaLogFile::$format, $log));
 			}else{
 			    if(is_array($info['message'])){
 			        fwrite ($f, ($buffered)
 			            ? $info
-			            : vsprintf (AnaLogFile::$format_after, print_r($info['message'],true)));
+			            : vsprintf (AnaLogFile::$format_after, print_r($log['message'],true)));
 			    }else{
 			        fwrite ($f, ($buffered)
 			            ? $info
-			            : vsprintf (AnaLogFile::$format_after, $info['message']));
+			            : vsprintf (AnaLogFile::$format_after, $log['message']));
 			    }
 			    
 			}
--- src/Log/Tp5Log.php
+++ src/Log/Tp5Log.php
@@ -1,16 +1,17 @@
 <?php
 /* 'log'  => [
- // 日志记录方式，内置 file socket 支持扩展
- 'type'  => 'HjCommon\Log\Tp5Log',//'HjFile',
- // 日志保存目录
- 'path'  => LOG_PATH,
- // 日志记录级别
- 'level' => ['log','error','sql','debug'],
- ], */
+	// 日志记录方式，内置 file socket 支持扩展
+	'type'  => 'HjCommon\Log\Tp5Log',//'HjFile',
+	// 日志保存目录
+	'path'  => LOG_PATH,
+	// 日志记录级别
+	'level' => ['log','error','sql','debug'],
+], */
 
 
 namespace HjCommon\Log;
 
+use think\App;
 
 /**
  * 本地化调试输出到文件
@@ -19,12 +20,11 @@ class Tp5Log
 {
     protected $config = [
         'time_format' => 'Y-m-d H:i:s',
-        'file_size'   => 100971520,
+        'file_size'   => 20971520,
         'path'        => LOG_PATH,
-        'trace'        => true,
         'apart_level' => [],
     ];
-    
+
     // 实例化并传入参数
     public function __construct($config = [])
     {
@@ -32,7 +32,7 @@ class Tp5Log
             $this->config = array_merge($this->config, $config);
         }
     }
-    
+
     /**
      * 日志写入接口
      * @access public
@@ -44,50 +44,44 @@ class Tp5Log
     {
         $now         = date($this->config['time_format']);
         $destination = $this->config['path'] . date('Y-m-d') . '.log';
-        
-        if(!$this->config['trace']){
-            return ;
-        }
+
         $path = dirname($destination);
         !is_dir($path) && mkdir($path, 0755, true);
-        
+
         //检测日志文件大小，超过配置大小则备份日志文件重新生成
         if (is_file($destination) && floor($this->config['file_size']) <= filesize($destination)) {
             rename($destination, dirname($destination) . DS . $_SERVER['REQUEST_TIME'] . '-' . basename($destination));
         }
-        
+
         $depr = $depr ? "---------------------------------------------------------------\r\n" : '';
         $info = '';
-        //         if (App::$debug) {
-        // 获取基本信息
-        if (isset($_SERVER['HTTP_HOST'])) {
-            $current_uri = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-        } else {
-            $current_uri = "cmd:" . implode(' ', $_SERVER['argv']);
+        if (App::$debug) {
+            // 获取基本信息
+            if (isset($_SERVER['HTTP_HOST'])) {
+                $current_uri = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+            } else {
+                $current_uri = "cmd:" . implode(' ', $_SERVER['argv']);
+            }
+
+            $runtime    = round(microtime(true) - THINK_START_TIME, 10);
+            $runtimeMS    = ($runtime*1000)."ms";
+            $reqs       = $runtime > 0 ? number_format(1 / $runtime, 2) : '∞';
+            $time_str   = ' [运行时间：' . number_format($runtime, 6) . 's][吞吐率：' . $reqs . 'req/s]';
+            $memory_use = number_format((memory_get_usage() - THINK_START_MEM) / 1024, 2);
+            $memory_str = ' [内存消耗：' . $memory_use . 'kb]';
+            $file_load  = ' [文件加载：' . count(get_included_files()) . ']';
+
+            $info   = '=> [ log ] ' . $current_uri . $time_str . $memory_str . $file_load . "\r\n";
+            $server = isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '0.0.0.0';
+            $server_name = isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'http://';
+            $server_name = isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : $server_name;
+            $server_name = isset($_SERVER['HTTP_X_FORWARDED_SERVER']) ? $_SERVER['HTTP_X_FORWARDED_SERVER'] : $server_name;
+            $remote = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '0.0.0.0';
+            $remote = isset($_SERVER['HTTP_X_REAL_IP']) ? $_SERVER['HTTP_X_REAL_IP'] : $remote;
+            $remote = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $remote;
+            $method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'CLI';
+            $uri    = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
         }
-        
-        $runtime    = round(microtime(true) - THINK_START_TIME, 10);
-        $runtimeMS    = ($runtime*1000)."ms";
-        $reqs       = $runtime > 0 ? number_format(1 / $runtime, 2) : '∞';
-        $time_str   = ' [运行时间：' . number_format($runtime, 6) . 's][吞吐率：' . $reqs . 'req/s]';
-        $memory_use = number_format((memory_get_usage() - THINK_START_MEM) / 1024, 2);
-        $memory_str = ' [内存消耗：' . $memory_use . 'kb]';
-        $file_load  = ' [文件加载：' . count(get_included_files()) . ']';
-        
-        $info   = '=> [ log ] ' . $current_uri . $time_str . $memory_str . $file_load . "\r\n";
-        $server = isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '0.0.0.0';
-        $server_name = isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'http://';
-        $server_name = isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : $server_name;
-        $server_name = isset($_SERVER['HTTP_X_FORWARDED_SERVER']) ? $_SERVER['HTTP_X_FORWARDED_SERVER'] : $server_name;
-        $remote = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '0.0.0.0';
-        $remote = isset($_SERVER['HTTP_X_REAL_IP']) ? $_SERVER['HTTP_X_REAL_IP'] : $remote;
-        $remote = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $remote;
-        if(strpos($remote, ',')!==false){
-            $remote = trim(explode(',', $remote)[0]);
-        }
-        $method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'CLI';
-        $uri    = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
-        //         }
         foreach ($log as $type => $val) {
             $level = '';
             foreach ($val as $msg) {
@@ -104,11 +98,11 @@ class Tp5Log
                 $info .= $level;
             }
         }
-        //         if (App::$debug) {
-        //             $info = "[{$server}][{$remote}] {$method} {$uri}\r\n" . $info;[Domain:{$server_name}]
-        $info = "[IP:{$remote}][Runtime:{$runtimeMS}][Domain:{$server_name}]: \r\n=> [ url ] {$uri} \r\n" . $info;
-        //         }
+        if (App::$debug) {
+//             $info = "[{$server}][{$remote}] {$method} {$uri}\r\n" . $info;[Domain:{$server_name}]
+            $info = "[IP:{$remote}][URL:{$uri}][Runtime:{$runtimeMS}][Domain:{$server_name}]: \r\n" . $info;
+        }
         return error_log("[{$now}]{$info} \r\n", 3, $destination);
     }
-    
+
 }
